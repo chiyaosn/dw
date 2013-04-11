487aae2570d42e58252bb8e04889629673702003
Chi Yao <chi.yao@service-now.com>
2013-04-11 13:16:32 -0700

    basic form input for host and metrics

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,16 +2,41 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import play.data.*;
+import static play.data.Form.*;
 import views.html.*;
+import models.*;
 
 public class Application extends Controller {
   
+    final static Form<Query> queryForm = new Form(Query.class);
+
     public static Result index() {
         //return ok(index.render("My new application is ready."));
-        String url = "http://10.64.8.75:4242";
+        
+	// show default chart
+	String query = "http://10.64.8.75:4242/q?start=15m-ago&ignore=48&m=sum:servlet.concurrents";
 
-        return ok(chart.render(url));
+        return ok(chart.render(query,queryForm));
     }
-  
+
+    public static Result chart() {
+
+	Form<Query> filledForm = queryForm.bindFromRequest();
+        String host = filledForm.field("host").valueOr("");  // e.g. 10.64.8.75:4242
+	String metrics = filledForm.field("metrics").valueOr(""); // e.g. sum:servlet.concurrents
+        if (host.isEmpty() || metrics.isEmpty()) {
+	    return ok("Missing host or metrics");
+            // TODO: to handle better
+	}
+	//String window = "start=2013/03/21-12:00:00&end=2013/03/22-12:00:00";
+	String window = "start=15m-ago&ignore=48";
+
+        String query = new StringBuilder("http://").
+	    append(host).append("/q?").append(window).append("&m=").
+	    append(metrics).toString();
+	
+	return ok(chart.render(query,filledForm));
+    }
+
 }
---
+++ app/models/Query.java
@@ -0,0 +1,11 @@
+package models;
+
+import javax.validation.*;
+
+import play.data.validation.Constraints.*;
+
+public class Query {
+
+    public String host;
+    public String metrics;
+}
