046e6894aae9b13c1f7feaeb2d2c35c89e98f457
SERVICE-NOW\eason.hu <SERVICE-NOW\eason.hu@msjcc02k60ekdkq4.corp.service-now.com>
2013-04-12 10:47:35 -0700

    Reformatted the code, added timewindow inputbox

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,40 +3,39 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import play.data.*;
+
 import static play.data.Form.*;
+
 import views.html.*;
 import models.*;
 
 public class Application extends Controller {
-  
-    final static Form<Query> queryForm = new Form(Query.class);
+
+    final static Form<Query> queryForm = form(Query.class);
 
     public static Result index() {
         //return ok(index.render("My new application is ready."));
-        
-	// show default chart
-	String query = "http://10.64.8.75:4242/q?start=15m-ago&ignore=48&m=sum:servlet.concurrents";
 
-        return ok(chart.render(query,queryForm));
+        // show default chart
+        Query query = new Query("10.64.8.75:4242", "sum:servlet.concurrents", "15m");
+
+        return ok(chart.render(query, queryForm));
     }
 
     public static Result chart() {
+        Form<Query> filledForm = queryForm.bindFromRequest();
+        Query query = filledForm.get();
+        //String timewindow = "start=2013/03/21-12:00:00&end=2013/03/22-12:00:00";
 
-	Form<Query> filledForm = queryForm.bindFromRequest();
-        String host = filledForm.field("host").valueOr("");  // e.g. 10.64.8.75:4242
-	String metrics = filledForm.field("metrics").valueOr(""); // e.g. sum:servlet.concurrents
-        if (host.isEmpty() || metrics.isEmpty()) {
-	    return ok("Missing host or metrics");
-            // TODO: to handle better
-	}
-	//String window = "start=2013/03/21-12:00:00&end=2013/03/22-12:00:00";
-	String window = "start=15m-ago&ignore=48";
+        //String host = filledForm.field("host").valueOr("");         // e.g. 10.64.8.75:4242
+        //String metric = filledForm.field("metric").valueOr("");     // e.g. sum:servlet.concurrents
 
-        String query = new StringBuilder("http://").
-	    append(host).append("/q?").append(window).append("&m=").
-	    append(metrics).toString();
-	
-	return ok(chart.render(query,filledForm));
+        if(query.host.isEmpty() || query.metric.isEmpty()) {
+            return ok("Missing host or metrics");
+            // TODO
+        }
+
+        //return ok(queryData.host + queryData.metric + queryData.timewindow);
+        return ok(chart.render(query, filledForm));
     }
-
 }
--- app/models/Query.java
+++ app/models/Query.java
@@ -5,7 +5,23 @@ import javax.validation.*;
 import play.data.validation.Constraints.*;
 
 public class Query {
-
     public String host;
-    public String metrics;
+    public String metric;
+    public String timewindow;
+
+    public Query() {
+        host = "";
+        metric = "";
+        timewindow = "";
+    }
+
+    public Query(String h, String m, String tw) {
+        host = h;
+        metric = m;
+        timewindow = tw;
+    }
+
+    public String GetQueryString() {
+        return "http://" + host + "/q?start=" + timewindow + "-ago&m=" + metric;
+    }
 }
